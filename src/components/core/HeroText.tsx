e49b986a3475b4353d440dd42cba526520f82190
ajaykrupalk <ajaykrupalk@outlook.in>
2024-08-31 09:47:06 +0530

    fixed search and added group hover

--- src/components/core/HeroText.tsx
+++ src/components/core/HeroText.tsx
@@ -5,12 +5,12 @@ export default function HeroText() {
     return (
         <>
             <div className="w-full flex justify-center">
-                <div className='outline-none w-fit text-xs bg-gray-100 h-full p-1.5 rounded-full font-medium hover:ring-2 hover:ring-indigo-200'>
+                <div className='outline-none w-fit text-xs bg-gray-100 h-full p-1.5 rounded-full font-medium hover:ring-2 hover:ring-slate-200 group'>
                     <span className="peer">
                         <span className='text-xs bg-slate-900 text-white rounded-full py-0.5 px-2 mr-1.5'>New</span>
                         Notes for Big O Notation is live! 🎉
                     </span>
-                    <svg className='transition-transform duration-500 transform peer-hover:translate-x-0.5 hover:translate-x-0.5 ml-0.5 inline-flex' xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <svg className='transition-transform duration-500 transform group-hover:translate-x-0.5 ml-0.5 inline-flex' xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                         <path d="M9 6l6 6l-6 6" />
                     </svg>
@@ -27,9 +27,9 @@ export default function HeroText() {
             </div>
             <div className="mt-3">
                 <Link to="/docs/introduction">
-                    <Button className="w-fit h-fit">
-                        <span className="peer">Get Started</span>
-                        <svg className='transition-transform duration-500 transform peer-hover:translate-x-0.5 hover:translate-x-0.5 ml-0.5 inline-flex' xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <Button className="w-fit h-fit group">
+                        Get Started
+                        <svg className='transition-transform duration-300 transform group-hover:translate-x-1 ml-0.5 inline-flex' xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                             <path d="M9 6l6 6l-6 6" />
                         </svg>
---
+++ src/components/core/SearchBar.tsx
@@ -0,0 +1,84 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import {
+    Dialog,
+    DialogContent,
+} from "@/components/ui/dialog"
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+    CommandSeparator,
+    CommandShortcut,
+} from "@/components/ui/command"
+import { Search, Settings, User, CreditCard, PlusCircle } from 'lucide-react'
+
+export default function SearchDialog() {
+    const [open, setOpen] = useState(false)
+
+    useEffect(() => {
+        const down = (e: KeyboardEvent) => {
+            if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+                e.preventDefault()
+                setOpen((open) => !open)
+            }
+        }
+
+        document.addEventListener('keydown', down)
+        return () => document.removeEventListener('keydown', down)
+    }, [])
+
+    return (
+        <div className="w-full max-w-sm mx-auto">
+            <button className="w-full flex justify-between rounded-md py-1 pl-3 pr-1 text-slate-500 border border-slate-200 bg-slate-50/80 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50" onClick={() => setOpen(true)}>
+                <span className="mr-8 font-normal text-[13px]">Search for documentation...</span>
+                <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-normal text-muted-foreground opacity-100">
+                    <span className="text-xs">⌘</span>K
+                </kbd>
+            </button>
+
+            <Dialog open={open} onOpenChange={setOpen}>
+                <DialogContent className="p-0">
+                    <Command>
+                        <CommandInput placeholder="Type a command or search..." />
+                        <CommandList>
+                            <CommandEmpty>No results found.</CommandEmpty>
+                            <CommandGroup heading="Suggestions">
+                                <CommandItem>
+                                    <Search className="mr-2 h-4 w-4" />
+                                    <span>Search...</span>
+                                </CommandItem>
+                                <CommandItem>
+                                    <PlusCircle className="mr-2 h-4 w-4" />
+                                    <span>New Project</span>
+                                </CommandItem>
+                            </CommandGroup>
+                            <CommandSeparator />
+                            <CommandGroup heading="Settings">
+                                <CommandItem>
+                                    <User className="mr-2 h-4 w-4" />
+                                    <span>Profile</span>
+                                    <CommandShortcut>⌘P</CommandShortcut>
+                                </CommandItem>
+                                <CommandItem>
+                                    <CreditCard className="mr-2 h-4 w-4" />
+                                    <span>Billing</span>
+                                    <CommandShortcut>⌘B</CommandShortcut>
+                                </CommandItem>
+                                <CommandItem>
+                                    <Settings className="mr-2 h-4 w-4" />
+                                    <span>Settings</span>
+                                    <CommandShortcut>⌘S</CommandShortcut>
+                                </CommandItem>
+                            </CommandGroup>
+                        </CommandList>
+                    </Command>
+                </DialogContent>
+            </Dialog>
+        </div>
+    )
+}
